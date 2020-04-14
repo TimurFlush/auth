bb72bee22073d6254f893ccbc142fedf73d0766a
Flush <no@email.com>
2020-04-14 20:20:18 +0500

    #18 The variable has been moved

--- src/Policy/PolicyManager.php
+++ src/Policy/PolicyManager.php
@@ -111,9 +111,9 @@ class PolicyManager implements PolicyManagerInterface
      */
     protected function resolveComplexPolicy($owner): object
     {
-        $owner = is_object($owner) ? get_class($owner) : gettype($owner);
-
         if (!is_string($owner) && !is_object($owner)) {
+            $owner = is_object($owner) ? get_class($owner) : gettype($owner);
+
             throw new InvalidArgumentException(
                 sprintf(
                     'The owner of a resolvable policy must be a string or an object, %s given',
